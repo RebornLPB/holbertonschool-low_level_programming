4485869796a4f0b979aaee7042d2b69c844a581c
Dylan <reborndiscord@gmx.fr>
2026-06-23 14:56:44 +0200

    Add 0-positive_or_negative.c to print a random number and its sign

---
+++ variable_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+* main - entry point
+*
+* Description: Print a random number and say if it's positive or negative or 0.
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else
+	{
+		printf("%d is 0\n", n);
+	}
+	return (0);
+}

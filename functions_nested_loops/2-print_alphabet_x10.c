4d8011d5d0cbb92362c8709c0861d8a39e8a2d95
Dylan <reborndiscord@gmx.fr>
2026-06-24 13:24:36 +0200

    Fix loop condition in print_alphabet_x10 and remove unnecessary function call

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,14 @@
 void print_alphabet_x10(void)
 {
 	int qty;
-	
-	for (qty = 0; qty <= 10; qty++)
+	char letter;
+
+	for (qty = 0; qty < 10; qty++)
 	{
-		print_alphabet();
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }

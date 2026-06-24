c3245f9342b9871639b3c90e7587848b277e0688
Dylan <reborndiscord@gmx.fr>
2026-06-24 13:19:59 +0200

    Fix function documentation and update header for print_alphabet_x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - Print alphabet function
+ * print_alphabet_x10 - Print alphabet but x10 function
  *
- * Description: Print alphabet and jump to next line.
+ * Description: Print alphabet x10 and jump to next line.
  */
 
 void print_alphabet_x10(void)
 {
 	int qty;
+	
 	for (qty = 0; qty <= 10; qty++)
 	{
 		print_alphabet();

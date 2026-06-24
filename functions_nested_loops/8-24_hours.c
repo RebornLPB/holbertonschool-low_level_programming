079b4f42b51df51cb6fed090e4d5186b512c6d76
Dylan <reborndiscord@gmx.fr>
2026-06-24 15:22:29 +0200

    Fix jack_bauer function to iterate over hours and minutes correctly

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,9 +12,9 @@ void jack_bauer(void)
 	int heures;
 	int minutes;
 
-	for (heures = 0; heures <= 24; heures++)
+	for (heures = 0; heures < 24; heures++)
 	{
-		for (minutes = 0; minutes <= 60; minutes++)
+		for (minutes = 0; minutes < 60; minutes++)
 		{
 			_putchar((heures / 10) + '0');
 			_putchar((heures % 10) + '0');

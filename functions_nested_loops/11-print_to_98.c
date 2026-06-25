dadb79b0d8ac3093dae179b5f9eabf896b372cb5
Dylan <reborndiscord@gmx.fr>
2026-06-25 10:36:03 +0200

    Add print_to_98 function to print natural numbers from n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98.
+ * @n: The starting integer.
+ *
+ * Description: Numbers are printed in order, separated by a comma
+ * and a space, and followed by a new line.
+ */
+
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d\n", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d\n", n);
+		n--;
+	}
+	printf("%d\n", n);
+}

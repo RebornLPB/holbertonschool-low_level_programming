71dd98b36462fdebd929a44ec4f955c0da00ae32
Dylan <reborndiscord@gmx.fr>
2026-06-25 11:51:07 +0200

    Add multiplication_table.c to implement a function that prints a multiplication table

---
+++ intro_debugging/multiplication_table.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * print_table - Prints a multiplication table of a given size.
+ * @size: The size of the table (number of rows and columns).
+ *
+ * Return: void.
+ */
+
+void print_table(int size)
+{
+	int row, col;
+
+	for (row = 1; row <= size; row++)
+	{
+		for (col = 1; col <= size; col++)
+		{
+			printf("%d ", row * col);
+		}
+		printf("\n");
+	}
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success).
+ */
+/* -- DO NOT Modify the code below this line -- */
+int main(void)
+{
+	print_table(4);
+	return (0);
+}

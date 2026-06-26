42197441ea068f683fb46fe384ab1869fcec3799
Dylan <reborndiscord@gmx.fr>
2026-06-26 15:00:38 +0200

    Fix formatting in print_square function documentation and code

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -2,12 +2,13 @@
 
 /**
  * print_square - prints a square, followed by a new line.
+ * @size: the size of the square
  */
 
 void print_square(int size)
 {
 	int row, col;
-
+	
 	if (size <= 0)
 	{
 		_putchar('\n');

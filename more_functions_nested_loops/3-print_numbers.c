743c438540db33f7a3e716739d89e022c45f897a
Dylan <reborndiscord@gmx.fr>
2026-06-26 10:20:11 +0200

    Add print_numbers function to print digits from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers, from 0 to 9, followed by a new line.
+ *
+ * Return: numbers: from 0 to 9
+ */
+
+
+void print_numbers(void)
+{
+	int value;
+
+	for (value = '0'; value <= '9'; value++)
+	{
+		_putchar(value);
+	}
+	_putchar('\n');
+}

bf8fd66bf4ca31cec03c7073e9243ee81ce2ec0c
Dylan <reborndiscord@gmx.fr>
2026-06-26 10:14:09 +0200

    Add _mul function to multiply two integers and update main.h

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _mul - Multiply two integers.
+ * @o: The first value
+ * @t: The first value
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+
+int _mul(int o, int t)
+{
+	int result = o * t;
+
+	return (result);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,7 +5,7 @@
 
 int _isupper(int);
 int _isdigit(int c);
-
+int mul(int a, int b);
 
 
 #endif

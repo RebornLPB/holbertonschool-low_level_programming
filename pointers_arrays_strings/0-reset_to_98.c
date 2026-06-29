0e857ae97349c8f5b92f82ab43c2985427711d7f
Dylan <reborndiscord@gmx.fr>
2026-06-29 15:59:47 +0200

    Refactor reset_to_98 function documentation and update main.h header file

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - Met à jour la valeur pointée par le pointeur à 98.
- * @n: Un pointeur vers un entier.
+ * reset_to_98 - Updates the value it points to to 98.
+ * @n: A pointer to an int.
+ *
+ * Return: void.
  */
 void reset_to_98(int *n)
 {

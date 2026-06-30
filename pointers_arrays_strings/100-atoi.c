51739b12a4c37fecbb3212b0a01be895ffc296d8
Dylan <reborndiscord@gmx.fr>
2026-06-30 15:50:00 +0200

    Add _atoi function to convert a string to an integer

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value of the converted string.
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int result = 0;
+	int signe = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			signe = signe * -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = ;
+		}
+		else
+		{
+			break;
+		}
+		i++;
+	}
+
+	if (signe >= 0)
+	{
+		return (-result);
+	}
+	return (result);
+}

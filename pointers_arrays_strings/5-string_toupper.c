3497265d362e02cf8e4b75edd86410c1b5d27885
Dylan <reborndiscord@gmx.fr>
2026-07-01 16:26:28 +0200

    Add string_toupper function to convert a string to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] -= 32;
+		}
+	i++;
+	}
+	return (str);
+}

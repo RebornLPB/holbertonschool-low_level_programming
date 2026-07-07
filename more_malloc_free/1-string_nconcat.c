5de655b7c7914589d158dd8e731651650f02bea7
Dylan <reborndiscord@gmx.fr>
2026-07-07 11:41:30 +0200

    Remove unnecessary blank lines in string_nconcat function for improved readability

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -29,35 +29,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		len1++;
 	}
-
 	while (s2[len2] != '\0')
 	{
 		len2++;
 	}
-
 	if (n >= len2)
 	{
 		n = len2;
 	}
-
 	result = malloc(sizeof(char) * (len1 + n + 1));
 
 	if (result == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < len1; i++)
 	{
 		result[i] = s1[i];
 	}
-
 	for (j = 0; j < n; j++)
 	{
 		result[i] = s2[j];
 		i++; 
 	}
-
 	result[i] = '\0';
 
 	return (result);

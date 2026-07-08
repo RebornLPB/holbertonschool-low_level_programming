4526d7f189276a69823480c8fc9de9e793b116d6
Dylan <reborndiscord@gmx.fr>
2026-07-08 10:36:54 +0200

    Add print_dog function to display struct dog information

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* print_dog - Prints a struct dog.
+* @d: A pointer to the struct dog to print.
+*
+* Description: If an element of d is NULL, it prints (nil) instead.
+* If d itself is NULL, it prints nothing.
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}

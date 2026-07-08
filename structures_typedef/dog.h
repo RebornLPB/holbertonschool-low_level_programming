029865cc8f391be224e945a753c9f906aedfa7c7
Dylan <reborndiscord@gmx.fr>
2026-07-08 10:06:17 +0200

    Add dog structure to define basic information about a dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure representing a dog
+ * @name: The name of the dog (string)
+ * @age: The age of the dog (floating point number)
+ * @owner: The name of the owner (string)
+ *
+ * Description: This structure holds basic information about a dog.
+ */
+
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+#endif

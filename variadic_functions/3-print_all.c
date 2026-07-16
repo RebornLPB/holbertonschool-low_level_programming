bd2ac161618ce075925eb7282d0d6b7444e13fcf
Dylan <reborndiscord@gmx.fr>
2026-07-16 11:22:22 +0200

    Refactor print_all function for improved readability and structure

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,47 +9,43 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	unsigned int i = 0;
+	unsigned int i = 0, j;
 	char *str, *sep = "";
+	const char t_types[] = "cifs";
 
 	va_start(ap, format);
+
 	if (format != NULL)
 	{
 		while (format[i] != '\0')
 		{
-			while (format[i] == 'c')
+			j = 0;
+			while (t_types[j] != '\0' && format[i] != t_types[j])
 			{
-				printf("%s%c", sep, va_arg(ap, int));
-				sep = ", ";
-				break;
+				j++;
 			}
-			while (format[i] == 'i')
+
+			while (t_types[j] != '\0')
 			{
-				printf("%s%d", sep, va_arg(ap, int));
-				sep = ", ";
-				break;
-			}
-			while (format[i] == 'f')
-			{
-				printf("%s%f", sep, va_arg(ap, double));
-				sep = ", ";
-				break;
-			}
-			while (format[i] == 's')
-			{
-				str = va_arg(ap, char *);
-				if (str == NULL)
+				if (format[i] == 'c')
+					printf("%s%c", sep, va_arg(ap, int));
+				if (format[i] == 'i')
+					printf("%s%d", sep, va_arg(ap, int));
+				if (format[i] == 'f')
+					printf("%s%f", sep, (float)va_arg(ap, double));
+				if (format[i] == 's')
 				{
-					str = "(nil)";
+					str = va_arg(ap, char *);
+					if (str == NULL)
+						str = "(nil)";
+					printf("%s%s", sep, str);
 				}
-				printf("%s%s", sep, str);
 				sep = ", ";
 				break;
 			}
 			i++;
 		}
 	}
-
 	va_end(ap);
 	printf("\n");
 }

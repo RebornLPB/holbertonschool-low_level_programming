d5cd06f32776fa17069a5aea30ddffae84413a1e
Dylan <reborndiscord@gmx.fr>
2026-07-16 11:24:27 +0200

    Refactor print_all function for clarity and remove unused main.h

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -9,43 +9,50 @@
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	unsigned int i = 0, j;
+	unsigned int i = 0;
 	char *str, *sep = "";
-	const char t_types[] = "cifs";
 
 	va_start(ap, format);
-
 	if (format != NULL)
 	{
 		while (format[i] != '\0')
 		{
-			j = 0;
-			while (t_types[j] != '\0' && format[i] != t_types[j])
-			{
-				j++;
-			}
-
-			while (t_types[j] != '\0')
+			while (1)
 			{
 				if (format[i] == 'c')
+				{
 					printf("%s%c", sep, va_arg(ap, int));
+					break;
+				}
 				if (format[i] == 'i')
+				{
 					printf("%s%d", sep, va_arg(ap, int));
+					break;
+				}
 				if (format[i] == 'f')
-					printf("%s%f", sep, (float)va_arg(ap, double));
+				{
+					printf("%s%f", sep, va_arg(ap, double));
+					break;
+				}
 				if (format[i] == 's')
 				{
 					str = va_arg(ap, char *);
 					if (str == NULL)
 						str = "(nil)";
 					printf("%s%s", sep, str);
+					break;
 				}
-				sep = ", ";
+				sep = "";
 				break;
 			}
+			if (format[i] == 'c' || format[i] == 'i' ||
+			    format[i] == 'f' || format[i] == 's')
+				sep = ", ";
+
 			i++;
 		}
 	}
+
 	va_end(ap);
 	printf("\n");
 }

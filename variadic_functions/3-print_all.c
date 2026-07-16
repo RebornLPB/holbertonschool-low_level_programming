bd26b234be1ab89d2ab00eda1ff1a0b8ff8e7ac1
Dylan <reborndiscord@gmx.fr>
2026-07-16 11:55:49 +0200

    Refactor print_all function to use a function pointer array for improved extensibility and readability

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,58 +1,89 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-#include <stdarg.h>
 
 /**
- * print_all - prints anything
- * @format: a list of types of arguments passed to the function
+ * print_char - Prints a character from variadic arguments
+ * @args: Pointer to variadic arguments list
+ */
+void print_char(va_list *args)
+{
+	printf("%c", va_arg(*args, int));
+}
+
+/**
+ * print_integer - Prints an integer from variadic arguments
+ * @args: Pointer to variadic arguments list
+ */
+void print_integer(va_list *args)
+{
+	printf("%d", va_arg(*args, int));
+}
+
+/**
+ * print_float - Prints a float from variadic arguments
+ * @args: Pointer to variadic arguments list
+ */
+void print_float(va_list *args)
+{
+	printf("%f", va_arg(*args, double));
+}
+
+/**
+ * print_string - Prints a string from variadic arguments
+ * @args: Pointer to variadic arguments list
+ */
+void print_string(va_list *args)
+{
+	char *str = va_arg(*args, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * print_all - Prints any combination of char, int, float, and string
+ * @format: A string where each character represents the type of the
+ *	 next variadic argument: 'c' for char, 'i' for integer,
+ *	 'f' for float, and 's' for string. NULL pointers for strings are
+ *	 printed as "(nil)".
+ *
+ * Description: The function parses the format string and for each
+ *	 recognized type, retrieves the corresponding argument from the
+ *	 variadic list and prints it. Values are separated by ", ".
  */
 void print_all(const char * const format, ...)
 {
-	va_list ap;
+	va_list all;
 	unsigned int i = 0;
-	char *str, *sep = "";
+	unsigned int n = 0;
+	char *seperator = "";
+	conv_t convs[] = {
+		{'c', print_char},
+		{'i', print_integer},
+		{'f', print_float},
+		{'s', print_string},
+		{0, NULL}
+	};
 
-	va_start(ap, format);
-	if (format != NULL)
+	va_start(all, format);
+
+	while (format && *(format + i))
 	{
-		while (format[i] != '\0')
+		n = 0;
+		while (convs[n].type != *(format + i) && convs[n].f != NULL)
 		{
-			while (1)
-			{
-				if (format[i] == 'c')
-				{
-					printf("%s%c", sep, va_arg(ap, int));
-					break;
-				}
-				if (format[i] == 'i')
-				{
-					printf("%s%d", sep, va_arg(ap, int));
-					break;
-				}
-				if (format[i] == 'f')
-				{
-					printf("%s%f", sep, va_arg(ap, double));
-					break;
-				}
-				if (format[i] == 's')
-				{
-					str = va_arg(ap, char *);
-					if (str == NULL)
-						str = "(nil)";
-					printf("%s%s", sep, str);
-					break;
-				}
-				sep = "";
-				break;
-			}
-			if (format[i] == 'c' || format[i] == 'i' ||
-			    format[i] == 'f' || format[i] == 's')
-				sep = ", ";
-
-			i++;
+			n++;
 		}
+		if (convs[n].f != NULL)
+		{
+			printf("%s", seperator);
+			convs[n].f(&all);
+			seperator = ", ";
+		}
+		i++;
 	}
 
-	va_end(ap);
+	va_end(all);
 	printf("\n");
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,6 +3,21 @@
 
 #include <stdarg.h>
 
+/**
+ * struct conv - Struct association format / function
+ * @type: The format character
+ * @f: The printing function associated
+ */
+typedef struct conv
+{
+	char type;
+	void (*f)(va_list *args);
+} conv_t;
+
+void print_char(va_list *args);
+void print_integer(va_list *args);
+void print_float(va_list *args);
+void print_string(va_list *args);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

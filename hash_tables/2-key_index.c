e292c087a4f7e2a141c9d59abea42182a735c44c
Dylan <reborndiscord@gmx.fr>
2026-07-28 14:42:03 +0200

    Add key_index function to map a key to an index in a hash table

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+#include <stddef.h>
+#include "1-djb2.c"
+
+/**
+ * key_index - maps a key to an index in the array of a hash table
+ * @key: the key to hash
+ * @size: the size of the array of the hash table
+ *
+ * Return: the index at which the key/value pair should be stored
+ *         (0 if key is NULL or size is 0)
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int result;
+
+	if (key == NULL || size == 0)
+		return (0);
+	result = hash_djb2(key);
+
+	return (result % size);
+}

1bfde190bde2bf4d1d3d0b27f7ec1cd1160cae27
Dylan <reborndiscord@gmx.fr>
2026-07-28 14:43:05 +0200

    Add missing newline for consistency in key_index function

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -15,6 +15,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 
 	if (key == NULL || size == 0)
 		return (0);
+
 	result = hash_djb2(key);
 
 	return (result % size);
